91c302637aba38b53259be367359857de6766684
Divyansh Sharma <divyanshsharma4507@gmail.com>
2024-09-24 00:44:08 +0530

    Error in email part

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -1,6 +1,6 @@
 import { resend } from "@/lib/resend";
 import VerificationEmail from "../../emails/VerificationEmail";
-import { ApiResponse } from '@/types/ApiResponse';
+import { ApiResponse } from "@/types/ApiResponse";
 
 export async function sendVerificationEmail(
   email: string,
@@ -8,15 +8,24 @@ export async function sendVerificationEmail(
   verifyCode: string
 ): Promise<ApiResponse> {
   try {
-    await resend.emails.send({
-      from: 'dev@hiteshchoudhary.com',
+    const response = await resend.emails.send({
+      from: "Acme <onboarding@resend.dev>",
       to: email,
-      subject: 'Mystery Message Verification Code',
+      subject: "Whisper Link | Verification Code",
       react: VerificationEmail({ username, otp: verifyCode }),
     });
-    return { success: true, message: 'Verification email sent successfully.' };
+
+    console.log("Resend API Response:", response);
+
+    return {
+      success: true,
+      message: "Verification email sent successfully.",
+    };
   } catch (emailError) {
-    console.error('Error sending verification email:', emailError);
-    return { success: false, message: 'Failed to send verification email.' };
+    console.error("Error sending verification email:", JSON.stringify(emailError, null, 2));
+    return {
+      success: false,
+      message: "An error occurred while sending the verification email.",
+    };
   }
 }
